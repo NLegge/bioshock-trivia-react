01839e837d4eb5aef50af225e01f2f9a2a77d7d4
Nicki Personal <little.legge@gmail.com>
2019-11-29 14:37:17 -0700

    Add reducer

--- src/utils/triviaQuestions.js
+++ src/utils/triviaQuestions.js
@@ -9,54 +9,44 @@ import {
   NewYears,
 } from '../assets/images';
 
-// import AynRand from '../assets/images';
-
 export const questions = [{
   question: 'Many thematic, architectural, and plot elements were strongly inspired by what famous author?',
-  answers: ['F Scott Fitzgerald', 'Ayn Rand', 'Ernest Hemingway', 'Edith Wharton'],
-  correctAnswer: 'Ayn Rand',
+  answers: [{ answer: 'F Scott Fitzgerald' }, { answer: 'Ayn Rand', isCorrect: true }, { answer: 'Ernest Hemingway' }, { answer: 'Edith Wharton' }],
   image: AynRand,
 },
 {
   question: 'Director Ken Lavine insisted on voicing which in game vending machine?',
-  answers: ['Gene Bank', 'El Ammo Bandito', 'Gatherers Garden', 'Circus of Values'],
-  correctAnswer: 'Circus of Values',
+  answers: [{ answer: 'Gene Bank' }, { answer: 'El Ammo Bandito' }, { answer: 'Gatherers Garden' }, { answer: 'Circus of Values', isCorrect: true }],
   image: Circus,
 },
 {
   question: 'What famous phrase allowed Frank Fontain to control Jack?',
-  answers: ['Abracadabra', 'If It Pleases', 'Would You Kindly', 'What Is Your Pleasure'],
-  correctAnswer: 'Would You Kindly',
+  answers: [{ answer: 'Abracadabra' }, { answer: 'If It Pleases' }, { answer: 'Would You Kindly', isCorrect: true }, { answer: 'What Is Your Pleasure' }],
   image: WYK,
 },
 {
   question: 'What creature is the original source of ADAM?',
-  answers: ['Sea Slug', 'Little Sister', 'Splicer', 'Brigid Tenenbaum'],
-  correctAnswer: 'Sea Slug',
+  answers: [{ answer: 'Sea Slug', isCorrect: true }, { answer: 'Little Sister' }, { answer: 'Splicer' }, { answer: 'Brigid Tenenbaum' }],
   image: Slug,
 },
 {
   question: 'Which character is designed after a 19th century diving suit?',
-  answers: ['Splicer', 'Frank Fontain', 'Big Dady', 'Handy Man'],
-  correctAnswer: 'Big Dady',
+  answers: [{ answer: 'Splicer' }, { answer: 'Frank Fontain' }, { answer: 'Big Dady', isCorrect: true }, { answer: 'Handy Man' }],
   image: BigDaddy,
 },
 {
   question: 'Bioshock is often referred to as a spiritual successor to what game?',
-  answers: ['Dead Space', 'Fallout', 'Bioshock Infinite', 'System Shock 2'],
-  correctAnswer: 'System Shock 2',
+  answers: [{ answer: 'Dead Space' }, { answer: 'Fallout' }, { answer: 'Bioshock Infinite' }, { answer: 'System Shock 2', isCorrect: true }],
   image: SystemShock,
 },
 {
   question: 'Which of the following is not a plasmid?',
-  answers: ['Insect Swarm', 'Blizzard', 'Incinerate', 'Cyclone Trap'],
-  correctAnswer: 'Blizzard',
+  answers: [{ answer: 'Insect Swarm' }, { answer: 'Blizzard', isCorrect: true }, { answer: 'Incinerate' }, { answer: 'Cyclone Trap' }],
   image: WinterBlast,
 },
 {
   question: 'What holiday were Rapture citicens celebrating when the city was hurled into a state of dystopia?',
-  answers: ['New Years Eve', 'Christmas', 'Fourth of July', 'Halloween'],
-  correctAnswer: 'New Years Eve',
+  answers: [{ answer: 'New Years Eve', isCorrect: true }, { answer: 'Christmas' }, { answer: 'Fourth of July' }, { answer: 'Halloween' }],
   image: NewYears,
 },
 ];
